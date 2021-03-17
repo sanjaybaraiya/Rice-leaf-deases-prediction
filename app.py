663ff49033a48d2429a9bb2d5b43c21ae73d06ce
sanjaybaraiya <69505546+sanjaybaraiya@users.noreply.github.com>
2021-03-17 16:45:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask,render_template,request
+import numpy as np
+import  os
+from tensorflow.keras.models import  load_model
+from tensorflow.keras.preprocessing import image
+from werkzeug.utils import  secure_filename
+
+#define the flask app
+app=Flask(__name__)
+#load the model
+model=load_model('model.h5')
+
+def model_predict(img_path,model):
+    test_image=image.load_img(img_path,target_size=(224,224))
+    test_image=image.img_to_array(test_image)
+    test_image=test_image/255
+    test_image=np.expand_dims(test_image,axis=0)
+    result=model.predict(test_image)
+    return result
+
+@app.route('/',methods=['GET'])
+def index():
+    return render_template('app.html')
+@app.route('/predict',methods=['GET','POST'])
+def upload():
+    if request.method=='POST':
+        #get the file from post request
+        f=request.files['file']
+
+        #save the file upload file
+
+        basepath=os.path.dirname(os.path.realpath('__fiie__'))
+        file_path=os.path.join(basepath,'uploads',secure_filename(f.filename))
+        f.save(file_path)
+
+        #make prediction
+        result=model_predict(file_path,model)
+
+        categories=['bacterial leaf blight','brown spot','leaf smut']
+
+        #process your result for human
+        pred_class=result.argmax()
+
+        output=categories[pred_class]
+        return output
+    return None
+if __name__=='__main__':
+    app.run(debug=False,port=5926)
+
